1044e25b64421b07a174e7421c7a698b573f1214
younes2005-del <67025948+younes2005-del@users.noreply.github.com>
2020-10-29 15:25:16 +0100

    Update js.js

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ function permutate(form) {
     console.log("younes")   
    
   
-    document.getElementById("results").innerHTML = "Nti hya l2admin";
+    document.getElementById("names").innerHTML = "Nti hya l2admin";
 
 
   }
